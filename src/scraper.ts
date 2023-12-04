515ecc8130e1f93099431494f3be243d5f919abe
Suman Barua <sumanbarua576@gmail.com>
2023-12-04 15:14:26 +0100

    Formulating collected urls

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,10 +1,12 @@
 import axios, { AxiosResponse } from "axios";
 import * as cheerio from "cheerio";
+import * as fs from "fs";
+import * as path from "path";
+
+// Donwload directory
+const downloadDir = "./downloaded_pages";
 
 export async function scrapeWebsite(url: string): Promise<void> {
-  // Donwload directory
-  const downloadDir = "./downloaded_pages";
-
   // Get website content
   const response: AxiosResponse<any, any> = await axios.get(url);
   const $: cheerio.CheerioAPI = cheerio.load(response.data);
@@ -18,5 +20,15 @@ export async function scrapeWebsite(url: string): Promise<void> {
     )
   );
 
-  console.log("pageLinks:", pageLinks);
+  // Traverse every page of sidebar
+  for (const pageLink of pageLinks) {
+    const pageUrl = new URL(pageLink, url).href;
+    const pageDir = path.join(downloadDir, path.dirname(pageLink));
+
+    if (!fs.existsSync(pageDir)) {
+      fs.mkdirSync(pageDir, { recursive: true });
+    }
+
+    console.log("PageURL:", pageUrl);
+  }
 }
