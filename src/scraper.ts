fbfa27597b2c0663224239911b0c519ed4b8b3b0
Suman Barua <sumanbarua576@gmail.com>
2023-12-04 15:42:32 +0100

    Downloading css files

--- src/scraper.ts
+++ src/scraper.ts
@@ -6,6 +6,11 @@ import * as path from "path";
 // Donwload directory
 const downloadDir = "./downloaded_pages";
 
+async function downloadFile(url: string, filePath: string): Promise<void> {
+  const response = await axios.get(url, { responseType: "arraybuffer" });
+  fs.writeFileSync(filePath, Buffer.from(response.data));
+}
+
 async function scrapePage(url: string, baseDir: string): Promise<void> {
   const response: AxiosResponse<any, any> = await axios.get(url);
   const $: cheerio.CheerioAPI = cheerio.load(response.data);
@@ -17,6 +22,14 @@ async function scrapePage(url: string, baseDir: string): Promise<void> {
 
   const pagePath = path.join(baseDir, "index.html");
   fs.writeFileSync(pagePath, response.data);
+
+  $('link[rel="stylesheet"]').each(
+    (_: number, element: cheerio.Element): void => {
+      const cssUrl = new URL($(element).attr("href")!, url).href;
+      const cssPath = path.join(baseDir, "styles", path.basename(cssUrl));
+      void downloadFile(cssUrl, cssPath);
+    }
+  );
 }
 
 export async function scrapeWebsite(url: string): Promise<void> {
