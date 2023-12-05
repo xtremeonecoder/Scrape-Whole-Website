84cd9adc81a6bec52c66c3e7eb750a17a716abcf
Suman Barua <sumanbarua576@gmail.com>
2023-12-05 11:55:59 +0100

    Implementation and refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import { scrapeWebsite } from "./scraper";
 
+/**
+ * The main function for initiating web scraping on the specified website.
+ * @returns {Promise<void>} A promise that resolves when web scraping is completed.
+ */
 async function main(): Promise<void> {
   try {
+    // Attempt to scrape the specified website
     await scrapeWebsite("https://books.toscrape.com/");
+
+    // Log success message when web scraping is completed without errors
     console.log("Web scraping completed successfully.");
   } catch (error) {
+    // Log an error message if an exception occurs during web scraping
     console.error("An error occurred during web scraping:", error);
   }
 }
--- src/scraper.ts
+++ src/scraper.ts
@@ -1,52 +1,127 @@
 import axios, { AxiosResponse } from "axios";
-import * as cheerio from "cheerio";
 import * as fs from "fs";
 import * as path from "path";
+import * as cheerio from "cheerio";
+
+// List of traversed page links
+const traversedPages: string[] = [];
 
 // Donwload directory
 const downloadDir = "./downloaded_pages";
 
 async function downloadFile(url: string, filePath: string): Promise<void> {
-  const response = await axios.get(url, { responseType: "arraybuffer" });
+  const linkDir = path.dirname(filePath);
+  const response: AxiosResponse<any, any> = await axios.get(url, {
+    responseType: "arraybuffer",
+  });
 
-  if (!fs.existsSync(path.dirname(filePath))) {
-    fs.mkdirSync(path.dirname(filePath), { recursive: true });
+  if (!fs.existsSync(linkDir)) {
+    fs.mkdir(linkDir, { recursive: true }, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+
+      fs.writeFileSync(filePath, Buffer.from(response.data));
+    });
+  } else {
+    fs.writeFileSync(filePath, Buffer.from(response.data));
   }
+}
 
-  fs.writeFileSync(filePath, Buffer.from(response.data));
+function downloadAllFiles(
+  $: cheerio.CheerioAPI,
+  selector: any,
+  attribute: string,
+  url: string
+): void {
+  $(selector).each((_: number, element: cheerio.Element): void => {
+    const fileUrl = new URL($(element).attr(attribute)!, url).href;
+    const filePath = `${downloadDir}${fileUrl.split(".com")[1]}`;
+
+    void downloadFile(fileUrl, filePath);
+    // console.log(`---- Downloaded: ${path.basename(filePath)}`);
+  });
 }
 
 async function scrapePage(url: string, baseDir: string): Promise<void> {
   const response: AxiosResponse<any, any> = await axios.get(url);
   const $: cheerio.CheerioAPI = cheerio.load(response.data);
 
+  // Extract and download CSS, Icons files
+  downloadAllFiles($, "link[href]", "href", url);
+
+  // Extract and download javascript files
+  downloadAllFiles($, "script[src]", "src", url);
+
+  // Extract and download image files
+  downloadAllFiles($, "img[src]", "src", url);
+
   // Save the HTML file
   if (!fs.existsSync(baseDir)) {
     fs.mkdirSync(baseDir, { recursive: true });
   }
 
-  const pagePath = path.join(baseDir, "index.html");
+  const fileName =
+    path.basename(url) !== "books.toscrape.com"
+      ? path.basename(url)
+      : "index.html";
+  const pagePath = path.join(baseDir, fileName);
   fs.writeFileSync(pagePath, response.data);
 
-  $('link[rel="stylesheet"]').each(
-    (_: number, element: cheerio.Element): void => {
-      const cssUrl = new URL($(element).attr("href")!, url).href;
-      const cssPath = path.join(baseDir, "styles", path.basename(cssUrl));
-      void downloadFile(cssUrl, cssPath);
-    }
+  // Recursively scrape linked pages
+  let linkedPages = Array.from(
+    new Set(
+      $(".col-sm-8.col-md-9 a[href]")
+        .map((_, el) => $(el).attr("href"))
+        .get()
+    )
   );
 
-  $("script[src]").each((_: number, element: cheerio.Element): void => {
-    const jsUrl = new URL($(element).attr("src")!, url).href;
-    const jsPath = path.join(baseDir, "scripts", path.basename(jsUrl));
-    void downloadFile(jsUrl, jsPath);
-  });
+  // Traverse every linked pages in the content
+  for (const linkedPage of linkedPages) {
+    const linkedUrl = new URL(linkedPage, url).href;
+    const linkedBaseDir = path.join(baseDir, path.dirname(linkedPage));
 
-  $("img[src]").each((_: number, element: cheerio.Element): void => {
-    const imgUrl = new URL($(element).attr("src")!, url).href;
-    const imgPath = path.join(baseDir, "images", path.basename(imgUrl));
-    void downloadFile(imgUrl, imgPath);
-  });
+    if (traversedPages.includes(linkedUrl)) continue;
+
+    console.log(`--- Downloading Sub Page: ${linkedUrl}`);
+
+    if (!fs.existsSync(linkedBaseDir)) {
+      fs.mkdirSync(linkedBaseDir, { recursive: true });
+    }
+
+    traversedPages.push(linkedUrl);
+
+    await scrapePage(linkedUrl, linkedBaseDir);
+    console.log(`--- Downloaded Sub Page: ${linkedUrl}`);
+    console.log("\n-----------------------------------------------\n");
+  }
+}
+
+async function extractUrlsFromCSS(cssPath: string): Promise<string[]> {
+  const cssContent = fs.readFileSync(cssPath, "utf-8");
+  const urlRegex = /url\(['"]?([^'"]+)['"]?\)/g;
+  const matches = cssContent.match(urlRegex);
+  return matches ? matches.map((match) => match.replace(urlRegex, "$1")) : [];
+}
+
+function downloadResourcesFromUrls(url: string, fileLinks: string[]): void {
+  for (const fileLink of fileLinks) {
+    const absoluteUrl = new URL(`/static/oscar/css/${fileLink}`, url).href;
+    const resourcePath = `${downloadDir}${absoluteUrl.split(".com")[1]}`;
+    void downloadFile(absoluteUrl, resourcePath);
+    console.log(`--- Downloaded File: ${path.basename(absoluteUrl)}`);
+  }
+}
+
+async function extractUrlsFromCSSAndDownloadFiles(url: string) {
+  const cssPath = path.join(downloadDir, "static/oscar/css", "styles.css");
+
+  if (fs.existsSync(cssPath)) {
+    const fileLinks = await extractUrlsFromCSS(cssPath);
+    await downloadResourcesFromUrls(url, fileLinks);
+    console.log("\n-----------------------------------------------\n");
+  }
 }
 
 export async function scrapeWebsite(url: string): Promise<void> {
@@ -57,21 +132,36 @@ export async function scrapeWebsite(url: string): Promise<void> {
   // Recursively scrape linked pages
   let pageLinks = Array.from(
     new Set(
-      $("a[href]")
+      $(".sidebar a[href]")
         .map((_, el) => $(el).attr("href"))
         .get()
     )
   );
+
+  // Add website home page
+  pageLinks = ["index.html", ...pageLinks];
 
   // Traverse every page of sidebar
   for (const pageLink of pageLinks) {
     const pageUrl = new URL(pageLink, url).href;
     const pageDir = path.join(downloadDir, path.dirname(pageLink));
 
+    if (traversedPages.includes(pageUrl)) continue;
+
     if (!fs.existsSync(pageDir)) {
       fs.mkdirSync(pageDir, { recursive: true });
     }
 
+    traversedPages.push(pageUrl);
+
+    console.log(`Downloading Page: ${pageUrl}`);
     await scrapePage(pageUrl, pageDir);
+    console.log(`Downloaded Page: ${pageUrl}`);
+    console.log("\n-----------------------------------------------\n");
   }
+
+  console.log("Downloading CSS Dependancies:");
+  extractUrlsFromCSSAndDownloadFiles(url);
+  console.log("Downloaded CSS Dependancies:");
+  console.log("\n-----------------------------------------------\n");
 }
